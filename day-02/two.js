3fc1bcddc5e745a00c6a305850d125be57ffef24
Frederic Houdemer <frederic.houdemer@smile.fr>
2025-12-02 18:44:08 +0100

    :sparkles: add day 2 solution

---
+++ day-02/two.js
@@ -0,0 +1,39 @@
+import * as fs from 'node:fs';
+
+const file = await fs.readFileSync("input.txt", "utf8");
+// const file = await fs.readFileSync("simple.txt", "utf8");
+
+let sum = 0;
+
+for (const pair of file.split(",")) {
+	const [one, two] = pair.split("-");
+
+	const low = parseInt(one);
+	const high = parseInt(two);
+
+	for (let i = low ; i <= high; i++) {
+		const stack = i.toString();
+
+		if (stack.slice(0, stack.length /2) === stack.slice(stack.length/2)) {
+			sum+=i;
+			continue;
+		}
+
+		// Check for each combination up to half of the string.
+		for (let l = 1; l < Math.ceil(stack.length/2); l++) {
+			if (stack.length % l !== 0) {
+				continue;
+			}
+
+			const needle = stack.slice(0,l);
+
+			// Repeat the needle until completion, to see if it matches the target.
+			if (needle.repeat(stack.length/needle.length) === stack) {
+				sum += i;
+			}
+		}
+	}
+}
+
+console.log(sum);
+
